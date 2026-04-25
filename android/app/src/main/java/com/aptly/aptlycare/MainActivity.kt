10e4d7fe06633119d3a1a8c129cc52992e4eb688
Sayed Muhaamed Aamir <sayedaamir@Sayeds-MacBook-Pro.local>
2026-04-25 13:05:42 +0530

    ios test flight released

--- android/app/src/main/java/com/aptly/aptlycare/MainActivity.kt
+++ android/app/src/main/java/com/aptly/aptlycare/MainActivity.kt
@@ -1,4 +1,4 @@
-package com.aptly
+package com.aptly.aptlycare
 
 import com.facebook.react.ReactActivity
 import com.facebook.react.ReactActivityDelegate
--- android/app/src/main/java/com/aptly/aptlycare/MainApplication.kt
+++ android/app/src/main/java/com/aptly/aptlycare/MainApplication.kt
@@ -1,4 +1,4 @@
-package com.aptly
+package com.aptly.aptlycare
 
 import android.app.Application
 import com.facebook.react.PackageList
